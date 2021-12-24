069609eee75d1a3696a7b997c71442bb3be0df33
GitHub Action <action@github.com>
2021-12-24 06:47:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/strangemonad/dbtideaplugin/MyBundle.kt
+++ src/main/kotlin/com/github/strangemonad/dbtideaplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.strangemonad.dbtideaplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/strangemonad/dbtideaplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.strangemonad.dbtideaplugin.services
+
+import com.github.strangemonad.dbtideaplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
